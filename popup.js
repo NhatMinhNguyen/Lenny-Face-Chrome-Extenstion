d2e7b45c1812beef4179022fda311b1a3e189be3
Nhat Nguyen <nguyennm@Miamioh.edu>
2021-04-24 14:21:34 -0400

    add more faces

--- popup.js
+++ popup.js
@@ -1,8 +1,24 @@
-let button = document.getElementById("test");
 
-button.innerHTML='(&#865;&#176; &#860;&#662; &#865;&#176;)';
+let faces = [
+    {
+        id: "lenny1",
+        code: [40, 32, 865, 176, 32, 860, 662, 32, 865, 176, 41, 10]
+    },
+    {
+        id: "lenny2",
+        code: [40, 32, 864, 176, 32, 863, 662, 32, 865, 176, 41]
+    },
+    {
+        id: "lenny3",
+        code: [40, 32, 865, 126, 32, 860, 662, 32, 865, 176, 41]
+    },
+    {
+        id: "lenny4",
+        code: [40, 32, 865, 664, 32, 860, 662, 32, 865, 664, 41]
+    },
 
-function copy(text){
+]
+function copy(text) {
     const ta = document.createElement('textarea');
     ta.style.cssText = 'opacity:0; position:fixed; width:1px; height:1px; top:0; left:0;';
     ta.value = text;
@@ -14,7 +30,17 @@ function copy(text){
 }
 
 
-button.addEventListener("click", async () => {
-    copy(String.fromCharCode(40, 32, 865, 176, 32, 860, 662, 32, 865, 176, 41, 10));
-});
+for (i = 0; i < faces.length; i++) {
 
+    const button = document.createElement('button');
+    document.body.appendChild(button);
+    button.setAttribute("id", `lenny${i}`);
+
+    let code = faces[i].code;
+
+    button.innerHTML = String.fromCharCode(...code);
+
+    button.addEventListener("click", async () => {
+        copy(String.fromCharCode(...code));
+    });
+}
